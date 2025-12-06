22becea63e84298bb2da0c9ffcb8c0aea35d897a
Abhay S Kulkarni <abhayskulkarni11@gmail.com>
2025-12-06 19:55:38 +0530

    registration page

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,19 +1,27 @@
 import { useState } from 'react'
 import './assets/css/styles.css'
-import Header from './components/Header.jsx'
 import Main from './components/Main.jsx'
+import Register from './components/Register.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
-
+import Login from './components/Login.jsx'
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -4,6 +4,7 @@ import Button from './Button.jsx'
 const Main = () => {
   return (
     <>
+      
       <div className="container">
         <div className="p-5 text-center bg-light-dark rounded">
           <h2 className="text-light">Welcome To Stock Prediction Portal</h2>
